1d7c6cd26ebdcb05d7222aa1020adf8664171115
James Savery <83395912+jamessavery@users.noreply.github.com>
2025-06-23 18:28:21 +0100

    Added spotless & basic exceptions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,16 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.compose.compiler) apply false
+    id("com.diffplug.spotless") version "7.0.4" apply false
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            ktlint("1.6.0")
+        }
+    }
 }
