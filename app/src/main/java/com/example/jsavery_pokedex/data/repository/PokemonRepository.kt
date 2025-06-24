5ae3c99c5d4f03ffc65fed7cd1a4cc8e3e50a870
James Savery <83395912+jamessavery@users.noreply.github.com>
2025-06-24 15:10:44 +0100

    flows are overkill here

--- app/src/main/java/com/example/jsavery_pokedex/data/repository/PokemonRepository.kt
+++ app/src/main/java/com/example/jsavery_pokedex/data/repository/PokemonRepository.kt
@@ -5,38 +5,42 @@ import com.example.jsavery_pokedex.data.model.Pokemon
 import com.example.jsavery_pokedex.data.model.PokemonResponse
 import com.example.jsavery_pokedex.domain.util.Result
 import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class PokemonRepositoryImpl @Inject constructor(
     private val remoteDataSource: PokemonDataSource,
 ) : PokemonRepository {
-    override fun getPokemonList(nextPage: Int): Flow<Result<PokemonResponse>> =
-        flow {
-            try {
-                remoteDataSource.getPokemonList(nextPage)?.let { response -> emit(Result.Success(response)) }
-                    ?: emit(Result.Error(NullPointerException()))
-            } catch (e: Exception) {
-                if (e is CancellationException) throw e
-                emit(Result.Error(e))
+    override suspend fun getPokemonList(nextPage: Int): Result<PokemonResponse> {
+        return try {
+            val response = remoteDataSource.getPokemonList(nextPage)
+            if (response != null) {
+                Result.Success(response)
+            } else {
+                Result.Error(NullPointerException())
             }
+        } catch (e: Exception) {
+            if (e is CancellationException) throw e
+            Result.Error(e)
         }
+    }
 
-    override fun getPokemonDetail(id: Int): Flow<Result<Pokemon>> =
-        flow {
-            try {
-                remoteDataSource.getPokemonDetail(id)?.let { response -> emit(Result.Success(response)) }
-                    ?: emit(Result.Error(NullPointerException()))
-            } catch (e: Exception) {
-                if (e is CancellationException) throw e
-                emit(Result.Error(e))
+    override suspend fun getPokemonDetail(id: Int): Result<Pokemon> {
+        return try {
+            val response = remoteDataSource.getPokemonDetail(id)
+            if (response != null) {
+                Result.Success(response)
+            } else {
+                Result.Error(NullPointerException())
             }
+        } catch (e: Exception) {
+            if (e is CancellationException) throw e
+            Result.Error(e)
         }
+    }
 }
 
 interface PokemonRepository {
-    fun getPokemonList(nextPage: Int): Flow<Result<PokemonResponse>>
+    suspend fun getPokemonList(nextPage: Int): Result<PokemonResponse>
 
-    fun getPokemonDetail(id: Int): Flow<Result<Pokemon>>
+    suspend fun getPokemonDetail(id: Int): Result<Pokemon>
 }
